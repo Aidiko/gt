9a2d10a8d75d121bf97a72d7ed6309061ea11368
Aidiko <aidiko963@gmail.com>
2026-07-09 17:33:04 +0600

    а

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,14 @@ import './Header.css'
 function Header() {
 	return (
 		<header>
-			<Link to="/">Home</Link>
+			<nav>
+				<Link to="/">Home</Link>
+			</nav>
+			<div className="header-text">
+				<h1>Vector Header</h1>
+				<p>Place your text here</p>
+				<p className="small">Place your text here</p>
+			</div>
 		</header>
 	)
 }
